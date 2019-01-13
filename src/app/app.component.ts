2c439a8f1f393e6166f41aad80d4a7171443415d
unknown <faisalkhan.chat@gmail.com>
2019-01-13 06:09:24 -0800

    angular 7+ simple form with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngRouting';
+  genders = ['Male', 'Female'];
+  public signUpForm: FormGroup;
+  constructor( ) { }
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      username: new FormControl(null),
+      email: new FormControl(null),
+      gender: new FormControl('Male')
+    });
+
+  }
+
+  onSubmitted() {
+    console.log(this.signUpForm);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { UserComponent } from './users/user/user.component';
 import { Routes, RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppRoutingModule } from './app.routing.module';
-import { AuthService } from './auth.service';
-import { AuthGuard } from './auth.guard';
-import { CanDeactivateGaurd } from './page-not-found/can-deactivate-gaurd.service';
-import { ErrorPageComponent } from './error-page/error-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+const inrRoute: Routes = [
+  { path: '', component: AppComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    UsersComponent,
-    UserComponent,
-    PageNotFoundComponent,
-    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(inrRoute)
   ],
-  providers: [AuthService, AuthGuard, CanDeactivateGaurd],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
