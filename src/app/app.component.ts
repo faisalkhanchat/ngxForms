bb66c77434b0e6c757ff55282c6854f377f32bd8
unknown <faisalkhan.chat@gmail.com>
2019-01-15 11:29:31 -0800

    custom email and form name validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
-
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,19 +8,22 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   title = 'ngRouting';
+  forbiddenUserNames = ['Cheris', 'Anna'];
   genders = ['male', 'female'];
   public signUpForm: FormGroup;
-  constructor( ) { }
+  constructor() { }
   ngOnInit() {
     this.signUpForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, [Validators.required]),
-        email: new FormControl(null, [Validators.required, Validators.email]),
+        username: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        email: new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
       }),
       gender: new FormControl('male'),
       hobbies: new FormArray([])
     });
-
+    this.signUpForm.valueChanges.subscribe(
+      (value) => console.log(value)
+    );
   }
 
   onAddHobby() {
@@ -29,7 +32,29 @@ export class AppComponent implements OnInit {
   }
 
   onSubmitted() {
-    console.log(this.signUpForm.value);
+    console.log(this.signUpForm);
   }
+  // {nameIsForbidden: true}
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return { 'nameIsForbidden': true };
+    }
+    return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@gmail.com') {
+          resolve({ 'emailIsForbidden': true });
+        } else {
+          resolve(null);
+        }
+
+      }, 1500);
+    });
+    return promise;
+  }
+
 
 }
